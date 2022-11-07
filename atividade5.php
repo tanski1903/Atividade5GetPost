3b4b0f969a21fd4b34972dd0b916f0f723f06bc3
MatheusBrasil19 <matheusbrasil862@gmail.com>
2022-11-07 20:00:49 -0300

    first commit

---
+++ atividade5.php
@@ -0,0 +1,9 @@
+<?php 
+  $num1 = $_GET['num1'];
+  $num2 = $_GET['num2'];
+  $num3 = $_GET['num3'];
+  
+  $media = ($num1 + $num2 + $num3)/3;
+  
+  echo "A média é: $media";
+ ?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>Atividade 5</title>
+ </head>
+ <body>
+ <form action='atividade5.php' method="get">
+  Nota 1: <input type="text" name="num1"><br>
+  Nota 2: <input type="text" name="num2"><br>
+  Nota 3: <input type="text" name="num3"><br>
+  <input type="submit">
+ </form>
+ </body>
+</html>
